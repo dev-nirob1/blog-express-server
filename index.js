a5fd4357a438213f2fd3f6962003c8674ef22a22
dev-nirob1 <dev-nirob1@gmail.com>
2024-09-17 21:14:46 +0600

    userRoleUpdate api and user Post api update

--- index.js
+++ index.js
@@ -52,19 +52,29 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/blog/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.findOne(query)
+      res.send(result)
+    })
+
     app.post('/blogs', async (req, res) => {
       const blogContent = req.body
       const result = await blogsCollection.insertOne(blogContent)
       res.send(result)
     })
 
-    app.patch('/blog/:id', async(req, res) => {
+    app.patch('/blog/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const options= {upsert: true}
+      const updatedBlogs = req.body;
+      const query = { _id: new ObjectId(id) }
+      const options = { upsert: true }
       const updateDoc = {
         $set: {}
       }
+      const result = await blogsCollection.updateOne(query, updateDoc, options)
+      res.send(result)
     })
 
     app.delete('/blog/:id', async (req, res) => {
@@ -81,9 +91,16 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/users/role/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const result = await usersCollection.findOne(query)
+      res.send(result)
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
-      const query = { email: 'user?.email' }
+      const query = { email: user?.email }
       const existingUser = await usersCollection.findOne(query)
       if (existingUser) {
         return res.status(400).send({ message: 'User already exists' });
@@ -92,29 +109,51 @@ async function run() {
       res.send(result)
     })
 
-    app.patch('/user/admin/:email', async (req, res) => {
+    app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
+      const userInfo = req.body;
       const query = { email: email }
+      const options = { upsert: true }
       const updateDoc = {
         $set: {
-          role: 'admin'
+          name: userInfo.name,
+          email: userInfo.email,
+          profileImage: userInfo.image,
+          address: userInfo.address || null,
+          phone: userInfo.phone || null,
+          dob: userInfo.dob || null,
+        }
+      }
+      const result = await usersCollection.updateOne(query, updateDoc, options)
+      res.send(result)
+    })
+
+    app.patch('/users/roleUpdate/:email', async (req, res) => {
+      const email = req.params.email;
+      const { role } = req.body
+      const query = { email: email }
+
+      const updateDoc = {
+        $set: {
+          role: role
         }
       }
       const result = await usersCollection.updateOne(query, updateDoc)
       res.send(result)
     })
 
-    app.patch('/user/author/:email', async (req, res) => {
-      const email = req.params.email;
-      const query = { email: email }
-      const updateDoc = {
-        $set: {
-          role: 'author'
-        }
-      }
-      const result = await usersCollection.updateOne(query, updateDoc)
-      res.send(result)
-    })
+    // app.patch('/user/author/:email', async (req, res) => {
+    //   const email = req.params.email;
+    //   const query = { email: email }
+    //   const options = { upsert: true }
+    //   const updateDoc = {
+    //     $set: {
+    //       role: 'author'
+    //     }
+    //   }
+    //   const result = await usersCollection.updateOne(query, updateDoc)
+    //   res.send(result)
+    // })
 
     app.delete('/user/:email', async (req, res) => {
       const email = req.params.email;
