206172094eb55db8be6252b1da1d0778f80bbb86
dev-nirob1 <dev-nirob1@gmail.com>
2024-12-12 01:38:55 +0600

    apis for update user info and pagination in dashboard for blogs

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 const corsOptions = {
   origin: ['http://localhost:5173', 'https://blog-express-a0e83.web.app'],
   credentials: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
@@ -35,27 +35,79 @@ async function run() {
     const reactionsCollection = client.db('blogs-db').collection('reactions');
 
     // Blogs-related APIs
+
     app.get('/blogs', async (req, res) => {
       const page = parseInt(req.query.page) || 1;
       const limit = parseInt(req.query.limit) || 10;
       const search = req.query.search || '';
 
-      const query = search
-        ? {
+      const query = {
+        approved: true,
+        ...(search
+          ? {
             $or: [
               { title: { $regex: search, $options: 'i' } },
               { content: { $regex: search, $options: 'i' } },
               { category: { $regex: search, $options: 'i' } },
             ],
           }
-        : {};
-
+          : {})
+      }
       const skip = (page - 1) * limit;
-      const blogs = await blogsCollection.find(query).skip(skip).limit(limit).toArray();
+      const blogs = await blogsCollection.find(query).sort({ approvedAt: -1 }).skip(skip).limit(limit).toArray();
       const total = await blogsCollection.countDocuments(query);
       res.send({ blogs, total });
     });
 
+    //get blogs filterd by category
+
+    app.get('/blogs/:category', async (req, res) => {
+      const category = req.params.category;
+      const query = { category: category, approved: true }
+      const result = await blogsCollection.find(query).sort({ approvedAt: -1 }).toArray()
+      res.send(result)
+    })
+
+    // blogs for pagination in dashboard
+    app.get('/dashboard/blogs', async (req, res) => {
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 5;
+      const skip = (page - 1) * limit;
+
+      const result = await blogsCollection
+        .find()
+        .sort({ postAt: -1 })
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+
+      const totalBlogs = await blogsCollection.countDocuments();
+      res.send({ totalBlogs, result });
+    });
+
+
+
+    //get recent blogs
+    app.get('/recentBlogs', async (req, res) => {
+      const result = await blogsCollection.find({ approved: true }).sort({ approvedAt: -1 }).limit(5).toArray();
+      res.send(result);
+    });
+
+    // get editors picked blogs 
+    app.get('/editorsPick', async (req, res) => {
+      const result = await blogsCollection.find({ editorsPick: true, approved: true }).sort({ approvedAt: -1 }).limit(5).toArray()
+      res.send(result)
+    })
+
+    //get blogs for specific authors by email
+    app.get('/blogs/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { 'author.email': email, approved: true }
+      const result = await blogsCollection.find(query).sort({ approvedAt: -1 }).toArray();
+      res.send(result)
+    })
+
+    //get single blogs by id
     app.get('/blog/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -66,12 +118,61 @@ async function run() {
       res.send(result);
     });
 
+    //post blogs
     app.post('/blogs', async (req, res) => {
       const blogContent = req.body;
       const result = await blogsCollection.insertOne(blogContent);
       res.send(result);
     });
 
+    //update blogs status to approve
+    app.patch('/blog/approve/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          approved: true,
+          approvedAt: new Date(),
+          denied: false,
+          status: 'approved'
+        }
+      }
+      const result = await blogsCollection.updateOne(query, updateDoc, { upsert: true })
+      res.send(result)
+    })
+
+    //update blogs status to deny
+    app.patch('/blog/deny/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          denied: true,
+          approved: false,
+          status: 'denied'
+        }
+      }
+      const result = await blogsCollection.updateOne(query, updateDoc, { upsert: true })
+      res.send(result)
+    })
+
+    // pick or remove from editors pick blog (dashboad)
+    app.patch('/blogs/editorsPick/:id', async (req, res) => {
+      const id = req.params.id;
+      const { editorsPick } = req.body;
+      console.log(editorsPick)
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          editorsPick: editorsPick
+        }
+      }
+      const result = await blogsCollection.updateOne(query, updateDoc, { upsert: true })
+      res.send(result)
+    })
+
+
+    // update blog 
     app.patch('/blog/:id', async (req, res) => {
       const id = req.params.id;
       const updatedBlogs = req.body;
@@ -83,12 +184,15 @@ async function run() {
       res.send(result);
     });
 
+    //delete a blog
     app.delete('/blog/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await blogsCollection.deleteOne(query);
       res.send(result);
     });
+
+    // ----------------------------------------------------------------------
 
     // Users-related APIs
     app.get('/users', async (req, res) => {
@@ -114,18 +218,20 @@ async function run() {
       res.send(result);
     });
 
-    app.put('/user/:email', async (req, res) => {
+    app.patch('/user/updateInfo/:email', async (req, res) => {
       const email = req.params.email;
       const userInfo = req.body;
+      console.log(userInfo.name, email, { email: email })
+
       const query = { email: email };
+      // console.log(email, userInfo, query)
       const updateDoc = {
         $set: {
           name: userInfo.name,
-          email: userInfo.email,
-          profileImage: userInfo.image,
           address: userInfo.address || null,
           phone: userInfo.phone || null,
           dob: userInfo.dob || null,
+          bio: userInfo.bio || '',
         },
       };
       const result = await usersCollection.updateOne(query, updateDoc, { upsert: true });
